391b7eb2d56644bd63f13fbffec8ef1f05ae5466
Dhayse Tito <dhaysetito@gmail.com>
2024-10-10 12:28:16 -0300

    Adicionando rede neural

---
+++ projeto_final/src/python/test_model.py
@@ -0,0 +1,14 @@
+from flair.models import TextClassifier
+from flair.data import Sentence
+
+# Carregar o modelo salvo
+classifier = TextClassifier.load('modelo_de_emocoes_lr0.1_bs64_ep10/final-model.pt')
+
+# Frases para testar
+nova_frase = Sentence("Hoje eu apanhei")
+
+# Prever o sentimento da frase
+classifier.predict(nova_frase)
+
+# Exibir a previsão
+print(nova_frase.labels)  # Retorna o sentimento e a probabilidade associada
